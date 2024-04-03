120e32285d76518b1a9e8f34c3704418d5b7c8e2
Omar <omarhisham400@yahoo.com>
2024-04-03 21:13:15 +0200

    up

--- diff_bot/launch/rsp.launch.py
+++ diff_bot/launch/rsp.launch.py
@@ -2,28 +2,40 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import xacro
 
 def generate_launch_description():
-    # Define the name of the URDF file and the package
-    urdf_file_name = 'robot.urdf'
+    # Define the URDF and package name
+    urdf_file_name = 'robot.urdf.xacro'
     package_name = "diff_bot"
 
-    # Get the path to the URDF file
-    urdf_file_path = os.path.join(get_package_share_directory(package_name), "urdf", urdf_file_name)
-
-    # Read the URDF file content
-    with open(urdf_file_path, 'r') as urdf_file:
-        urdf_content = urdf_file.read()
-
-    # Create the Robot State Publisher node
+    # Fetch the URDF file path
+    robot_desc_path = os.path.join(get_package_share_directory(package_name), "urdf", urdf_file_name)
+    
+    # Process the xacro file
+    xacro_file = xacro.process_file(robot_desc_path)
+    
+    # Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'use_sim_time': True, 'robot_description': urdf_content}]
+        name='robot_state_publisher_node',
+        parameters=[{'use_sim_time': True, 'robot_description': xacro_file.toxml()}],
+        output="screen"
+    )
+    
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher_node',
+        output="screen"
     )
 
-    # Create and return the LaunchDescription object
-    return LaunchDescription([robot_state_publisher_node])
-
+    # Create and return launch description object
+    return LaunchDescription(
+        [            
+            robot_state_publisher_node,
+            joint_state_publisher_node,
+        ]
+    )
